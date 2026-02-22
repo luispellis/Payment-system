8889d0a96ebe507e4ce56d59b6bca6dae350b08a
luispellis <luisfelipe_pellis@hotmail.com>
2026-02-22 20:03:35 -0300

    feat: adiciona polimorfismo em Pagamento com taxa default e implementações de CartaoCredito e Pix

---
+++ src/com/devpellis/pagamentos/model/CartaoCredito.java
@@ -0,0 +1,26 @@
+package com.devpellis.pagamentos.model;
+
+public record CartaoCredito(String numeroCartao, String titular, double valor ) implements Pagamento {
+
+    public CartaoCredito {
+
+        if (numeroCartao == null || numeroCartao.isBlank()){
+            throw new IllegalArgumentException("Número do cartão não pode ser vazio");
+        }
+
+        if (titular == null || titular.isBlank()){
+            throw new IllegalArgumentException("Títular não pode ser vazio");
+        }
+
+        if (valor <= 0){
+            throw new IllegalArgumentException("Valor deve ser maior que zero");
+        }
+    }
+
+    private static final double TAXA = 0.05;
+
+    @Override
+    public double percentualTaxa(){
+        return TAXA;
+    }
+}
